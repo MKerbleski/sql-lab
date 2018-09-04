400ed2134c8595c0adfce97d4e40e17558cd8052
mkerbleski <mkerbles@gmail.com>
2018-09-04 12:27:24 -0600

    includes day 2 example.

---
+++ knexfile.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './db/lambda.sqlite3', // update path
+    },
+    useNullAsDefault: true, // add this line
+  },
+};
